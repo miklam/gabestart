f93d3f7050177be7223cd6e436b41522b2dc1f01
Gabe Pérez <1930100+gabeweb@users.noreply.github.com>
2022-10-24 20:47:28 -0400

    Add files via upload
    Changed fonts to .ttf. Now displays date and time when scrobbling nothing.

--- main.js
+++ main.js
@@ -77,3 +77,25 @@ for (let i = 0; i < tabs.length; i++) {
     contents[i].classList.add("active");
   });
 }
+
+// Function to format 1 in 01
+const zeroFill = n => {
+  return ('0' + n).slice(-2);
+}
+
+// Creates interval
+const interval = setInterval(() => {
+  // Get current time
+  const now = new Date();
+  const weekday = ['Sun','Mon','Tue','Wed','Thu', 'Fri','Sat'];
+  const month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+  // Format date as in mm/dd/aaaa hh:ii:ss
+  const dateTime = ' ' + weekday[now.getDay()] + ' ' + month[now.getMonth()] + ' ' + zeroFill(now.getUTCDate()) + ', ' + now.getFullYear();
+  const timeDate = ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+
+  // Display the date and time on the screen using div#date-time
+  document.getElementById('date-time').innerHTML = dateTime;
+document.getElementById('time-date').innerHTML = timeDate;
+}, 1000);
+
